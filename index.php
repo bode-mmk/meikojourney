45adc572a8c318e038ae8465986f09c4ca8c861f
nahanahaha-legend <super.perl.user@gmail.com>
2018-06-29 02:49:11 +0900

    エモーティコンの処理を変えた

--- index.php
+++ index.php
@@ -13,28 +13,8 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 $sign = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 $events = $bot->parseEventRequest(file_get_contents('php://input'), $sign);
 
-// エモーティコンの置換準備
-$raw_code = [
-  '100001' => '100001',
-  '100002' => '100002',
-  '100003' => '100003',
-  '100004' => '100004',
-  '100005' => '100005',
-  '100006' => '100006',
-  '100007' => '100007',
-  '100008' => '100008',
-];
-
-$emoticon = [];
-foreach($raw_code as $code){
-  $bin = hex2bin(str_repeat('0', 8 - strlen($code)). $code);
-  $emoticon[$code] = mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
-}
-
 // 返答ファイルの読み込み
 $response_sentence = file("response_list");
-
-// error_log('message', 3, 'test.log');
 
 foreach ($events as $event) {
   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent) ||
@@ -49,10 +29,10 @@ foreach ($events as $event) {
 
     if($event->getText() === $response[0]){
       // 絵文字は置換する → (100007とか)
-      //$str = preg_replace('/\((\d{6})\)/', $emoticon['100004'], $response[1] ); みたいに本来はしたい……
-      $str = preg_replace_callback('/\((\d{6})\)/',
+      $str = preg_replace_callback('/\(([0-9A-F]{6})\)/',
         function($matches) use ($emoticon){
-          return $emoticon[$matches[1]];
+          $bin = hex2bin(str_repeat('0', 8 - strlen($matches[1])).$matches[1]);
+          return mb_convert_encoding($bin, 'UTF-8', 'UTF-32BE');
         },
         $response[1]
       );
